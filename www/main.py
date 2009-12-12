de32a1f61ac42ad57b410021e83eb06dd052da18
Dave Peck <code@davepeck.org>
2009-12-11 18:19:37 -0800

    IN PROGRESS: untested, but I think this completes the implementation of our service.

--- www/main.py
+++ www/main.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
+import sys
 import datetime
 
-import sys
 import wsgiref.handlers
 from django.utils import simplejson
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 
-import sharedutil
+
+#------------------------------------------------------------------------------
+# Helper Methods
+#------------------------------------------------------------------------------
+
+def BREAKPOINT():
+  import pdb
+  p = pdb.Pdb(None, sys.__stdin__, sys.__stdout__)
+  p.set_trace()
 
 def get_rid_of_microseconds(dt):
     return datetime.datetime(year=dt.year, month=dt.month, day=dt.day, hour=dt.hour, minute=dt.minute, second=dt.second)
@@ -19,115 +27,159 @@ def iso_utc_string(dt):
     # hacked the UTC mark in.
     return no_micros.isoformat() + "Z"
 
-def BREAKPOINT():
-  import pdb
-  p = pdb.Pdb(None, sys.__stdin__, sys.__stdout__)
-  p.set_trace()
 
-# DataStore classes
+#------------------------------------------------------------------------------
+# App Engine Models
+#------------------------------------------------------------------------------
+
 class User(db.Model):
     display_name = db.StringProperty()
-    profile_image_url = db.LinkProperty()
+    profile_image_url = db.LinkProperty()    
+    message = db.StringProperty()
+    message_time = db.DateTimeProperty()
 
 class UserService(db.Model):
+    KNOWN_SERVICE_TYPES = ['twitter', 'facebook']    
+    
     user = db.ReferenceProperty(User, collection_name = "services")
     display_name = db.StringProperty()
-    profile_image_url = db.LinkProperty()
+    profile_image_url = db.LinkProperty()    
     service_type = db.StringProperty()  # "twitter"
     id_on_service = db.IntegerProperty() # 12345
     friend_ids = db.ListProperty(int)   # [12345, 6789]
-
+    
+    @staticmethod
+    def get_or_insert_for_service_and_id(service_type, id_on_service):
+        if service_type not in KNOWN_SERVICE_TYPES:
+            raise Exception("Invalid service_type")
+        key_name = 'us-%s-%s' % (service_type, str(id_on_service))
+        user_service = UserService.get_or_insert(key_name = key_name)
+        user_service.service_type = service_type
+        user_service.id_on_service = id_on_service
+        if user_service.friend_ids is None:
+            user_service.friend_ids = []
+        return user_service
+        
+    def iter_friend_services(self):
+        return UserService.all().filter('service_type =', self.service_type).filter('id_on_service IN', self.friend_ids)
+    
+    def iter_friend_users(self):
+        for friend_service in self.iter_friend_services():
+            yield friend_service.user
+        
+    def iter_friend_updates(self):
+        for friend_user in self.iter_friend_users():
+            if friend_user.location_updates:
+                location_update = friend_user.location_updates[0]
+                update = {
+                    "display_name": friend_user.display_name,
+                    "profile_image_url": friend_user.profile_image_url,
+                    "latitude": location_update.location.lat,
+                    "longitude": location_update.location.lon,
+                    "update_time": iso_utc_string(location_update.update_time),
+                    "message": friend_user.message if friend_user.message else ""
+                }
+                yield update
+                
+    @staticmethod
+    def updates_for_user_services(user_services):
+        seen = {}
+        for user_service in user_services:
+            for update in user_service.iter_friend_updates():
+                key = (update["display_name"], update["profile_image_url"])
+                if key not in seen:
+                    seen[key] = True
+                    yield update
+        
 class LocationUpdate(db.Model):
-    user = db.ReferenceProperty(User, collection_name = "locations")
+    user = db.ReferenceProperty(User, collection_name = "location_updates")
     location = db.GeoPtProperty()
-    update_time = db.DateTimeProperty(auto_now_add=True)
+    update_time = db.DateTimeProperty()
     horizontal_accuracy = db.FloatProperty()
 
-# Handlers
+
+#------------------------------------------------------------------------------
+# UpdateHandler
+#------------------------------------------------------------------------------
+
 class UpdateHandler(webapp.RequestHandler):
-    @staticmethod
-    def location_update_dictionary(update):
-        location_update = sharedutil.LocationUpdateJSON()
-        location_update.twitter_username = update.twitter_username
-        location_update.twitter_full_name = update.twitter_full_name
-        location_update.twitter_profile_image_url = str(update.twitter_profile_image_url)
-        location_update.hashtag = update.hashtag
-        location_update.message = update.message
-        location_update.latitude = update.latitude
-        location_update.longitude = update.longitude
-        location_update.update_datetime = iso_utc_string(update.update_datetime)
-        return location_update
-
-    def get(self, hashtag):
-        try:
-            update_list = []
-            updates = LocationUpdate.all().filter('hashtag =', hashtag).fetch(100)
-            for update in updates:
-                update_list.append(HashTagHandler.location_update_dictionary(update))
-        except:
-            response = {'success': False, 'message': 'Datastore failure', 'call_again_seconds': 15, 'updates': []}
-        else:
-            response = {'success': True, 'message': 'OK', 'call_again_seconds': 15, 'updates': update_list}
-
-        self.response.headers['Content-Type'] = 'application/json'
-        callback = self.request.get("callback")
-        if callback:
-            data = '%s(%s);' % (callback, simplejson.dumps(response))
-            self.response.out.write(data)
-        else:
-            self.response.out.write(simplejson.dumps(response))
-            #sys.__stdout__.write(simplejson.dumps(response))
-            #sys.__stdout__.flush()
-
     def post(self):
-        response = {'success': False, 'message': 'Unforseen error condition.'}
         try:
-            data = simplejson.loads(self.request.body.decode('utf8'))
             user = None
-            update_queue = []
-            for service in data['services']:
-                key_name = '%s%s' % (service['service_type'], service['id_on_service'])
-                user_service = UserService.get_or_insert(key_name = key_name)
-                user_service.id_on_service = service['id_on_service']
-                user_service.service_type = service['service_type']
-                user_service.display_name = service['display_name']
-                user_service.profile_image_url = service['profile_image_url']
-                update_queue.append(user_service)
 
-                if not user and not user_service.user:
-                    user = User()
-                    #TODO: use Twitter data in case of conflict
+            # Read data and basic sandity check
+            data = simplejson.loads(self.request.body.decode('utf8'))            
+            services = data.get('services', None)
+            if not services:
+                raise Exception('You must include service information in your post.')                         
+            
+            # Handle information for each service
+            info_from = None 
+            user_services = []
+            for service in services:
+                user_service = UserService.get_or_insert_for_service_and_id(service['service_type'], service['id_on_service'])
+                user_service.display_name = service.get('display_name', user_service.display_name)
+                user_service.profile_image_url = service.get('profile_image_url', user_service.profile_image_url)
+                user_service.friend_ids = service.get('friends', user_service.friend_ids)
+                user_services.append(user_service)
+
+                if user is None:
+                    user = user_service.user                
+                    if user is None: 
+                        user = User()                        
+                user_service.user = user
+                
+                if info_from != "twitter":
                     user.display_name = user_service.display_name
-                    user.profile_image_url = user_service.profile_image_url
-                    #TODO: error checking
-                    user.put()
-                    user_service.user = user
-                elif user and not user_service.user:
-                    user_service.user = user
-
-#            update = LocationUpdate.get_or_insert(key_name = data['twitter_username'])
-#            update.twitter_username = data['twitter_username']
-#            update.twitter_full_name = data['twitter_full_name']
-#            update.twitter_profile_image_url = data['twitter_profile_image_url']
-#            update.hashtag = data['hashtag']
-#            update.latitude = data['latitude']
-#            update.longitude = data['longitude']
-#            update.message = data['message']
-#            update.update_datetime = datetime.datetime.utcnow()
-        except KeyError:
-            response = {'success': False, 'message': 'Malformed POST request.'}
-        else:
-            try:
-                #TODO: is there a better way to commit all updated instances?
-                for item in update_queue:
-                    item.put()
-            except:
-                response = {'success': False, 'message': 'Datastore timeout or other error.'}
+                    user.profile_image_url = user_serivce.profile_image_url
+                    info_from = user_service.service_type
+                user_services.append(user_service)
+                
+            # Handle location update, if included
+            location_update = None
+            latitude = float(data.get('latitude', 0.0))
+            longitude = float(data.get('longitude', 0.0))
+            horizontal_accuracy = float(data.get('horizontal_accuracy', 0.0))
+            if latitude and longitude:
+                # For now, only keep the most recent location
+                if user.location_updates:
+                    location_update = user.location_updates[0]
+                else:
+                    location_update = LocationUpdate(user = user)
+                location_update.location = db.GeoPt(latitude, longitude)
+                location_update.horizontal_accuracy = horizontal_accuracy
+                location_update.update_time = datetime.datetime.utcnow()
+            
+            # Handle message, if any:
+            message = data.get('message', None)
+            if message:
+                user.message = message
+                user.message_time = datetime.datetime.utcnow()
+                
+            # Attempt to save everything in the datastore... (failure will get caught)
+            user.put()
+            if location_update:
+                location_update.put()
+            db.put(user_services)            
+                
+            # Now cons up some updates, if they're desired...
+            want_updates = data.get('want_updates', False)
+            if want_updates:
+                updates = UserService.updates_for_user_services(user_services)
             else:
-                response = {'success': True, 'message': 'OK'}
+                updates = []                
+        except Exception, message:
+            result = {'success': False, 'message': 'Encountered an unexpected exception (%s)' % message}            
+        else:
+            result = {'success': True, 'message': 'OK', 'updates': updates}
         finally:
             self.response.headers['Content-Type'] = 'application/json'
-            self.response.out.write(simplejson.dumps(response))
+            self.response.out.write(simplejson.dumps(result))
+
+
+#------------------------------------------------------------------------------
+# Other Misc. Handlers
+#------------------------------------------------------------------------------
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
@@ -139,6 +191,11 @@ class ViewHandler(webapp.RequestHandler):
         # XXX TODO something real
         self.response.out.write('<html><head><title>[View Tag %s]</title></head><body><a style="font-size: 18pt; font-family:Helvetica Neue" href="tweetthespotone://%s/">Open TweetTheSpot with "%s"</a><br/></body></html>\n' % (hashtag, hashtag, hashtag))
 
+
+#------------------------------------------------------------------------------
+# Bootstrapping
+#------------------------------------------------------------------------------
+
 def main():
   application = webapp.WSGIApplication([('/', MainHandler),
                                        ('/api/1/update/', UpdateHandler),
@@ -146,7 +203,6 @@ def main():
                                        ]
                                        )
   wsgiref.handlers.CGIHandler().run(application)
-
 
 if __name__ == '__main__':
   main()
