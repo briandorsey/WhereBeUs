48bc1e796a0d62016811cdafe5085325a1baaa18
Brian Dorsey <briandorsey@gmail.com>
2010-01-08 20:33:04 -0800

    The API tests (minimal as they are) still work. Did some cleanup & removed a skipped test for an API we no longer have.

--- www/test/main_test.py
+++ www/test/main_test.py
@@ -6,9 +6,36 @@ import httplib2
 
 import sharedutil_test
 
-base_url = "http://localhost:8082"
-#base_url = "http://ourtweetspot.appspot.com"
+BASE_URL = "http://localhost:8082/"
+#BASE_URL = "http://ourtweetspot.appspot.com"
+UPDATE_URL = BASE_URL + 'api/1/update/'
 
+TEMPLATE_DATA = {
+    "services":
+    [
+        {
+            "service_type": "twitter",
+            "display_name": "Twitter Name",
+            "profile_image_url": "http://example.com/twitter/",
+            "id_on_service": 12345,
+            "friends": [67890, 999, 312],
+        },
+
+
+        {
+            "service_type": "facebook",
+            "display_name": "Facebook Name",
+            "profile_image_url": "http://example.com/facebook/",
+            "id_on_service": 23456,
+            # "friends" key is optional
+        },
+    ],
+
+    "want_updates": False,
+    "latitude": 77.33,
+    "longitude": 99.22,
+    "message": "My current message.",
+    }
 #TODO: startup test dev_appserver.py in it's own dir for the tests
 
 def check_json_response(response):
@@ -17,65 +44,22 @@ def check_json_response(response):
 
 def test_get_root():
     """make sure we can even talk to the web server"""
-    url = base_url + '/'
     h = httplib2.Http()
-    resp, content = h.request(url, 'GET')
-    print url
+    resp, content = h.request(BASE_URL, 'GET')
+    print BASE_URL
     print resp
     print content
     assert resp['status'] == '200'
 
-def test_get_updates():
-    py.test.skip("disabled for rewrite of req/response protocol")
-    url = base_url + '/api/1/hashtag/testtag/'
-    h = httplib2.Http()
-    resp, content = h.request(url, 'GET')
-    print url
-    assert resp['status'] == '200'
-    print resp
-    print content
-    json_response = simplejson.loads(content)
-    print json_response
-    check_json_response(json_response)
-    assert 'call_again_seconds' in json_response
-    assert 'updates' in json_response
-    assert len(json_response['updates']) == 0
 
 def test_post_update():
-    url = base_url + '/api/1/update/'
     h = httplib2.Http()
-    data = {
-        "services":
-        [
-            {
-                "service_type": "twitter",
-                "display_name": "Twitter Name",
-                "profile_image_url": "http://example.com/twitter/",
-                "id_on_service": 12345,
-                "friends": [67890, 999, 312],
-            },
-
-
-            {
-                "service_type": "facebook",
-                "display_name": "Facebook Name",
-                "profile_image_url": "http://example.com/facebook/",
-                "id_on_service": 23456,
-                # "friends" key is optional
-            },
-        ],
-
-        "want_updates": True,
-        "latitude": 77.33,
-        "longitude": 99.22,
-        "message": "My current message.",
-        }
-    json_data = simplejson.dumps(data)
+    json_data = simplejson.dumps(TEMPLATE_DATA)
     print json_data
-    resp, content = h.request(url,
+    resp, content = h.request(UPDATE_URL,
                             'POST', body=json_data,
                             headers={'content-type':'application/json'} )
-    print url
+    print UPDATE_URL
     print resp
     print content
     assert resp['status'] == '200'
@@ -84,7 +68,7 @@ def test_post_update():
     assert json_response['success'] == True
 
     """
-    url = base_url + '/api/1/hashtag/posttag/'
+    url = BASE_URL + 'api/1/hashtag/posttag/'
     resp, content = h.request(url, 'GET')
     print url
     assert resp['status'] == '200'
